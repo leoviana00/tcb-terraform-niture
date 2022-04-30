c13765c1bce7a992588e05742de19989f8da40e7
Leonardo Viana <leonardoviana00@hotmail.com>
2022-04-29 23:25:36 -0300

    update

--- scripts/notificacaoStageFalha.sh
+++ scripts/notificacaoStageFalha.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # curl -X POST -H 'Content-type: application/json' --data '{"text":"tudo deu certo na pipeline!"}' https://hooks.slack.com/services/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
-curl -s -X POST https://api.telegram.org/bot1414485105:AAHiZeQkDjAjZl8NzwsAuBWP-of5w8gEoe0/sendMessage -d chat_id=-522831588 -d parse_mode="HTML" -d text="
+curl -s -X POST https://api.telegram.org/<token-bot>/sendMessage -d chat_id=<chat-id>  -d parse_mode="HTML" -d text="
 <b>Date</b>: $(date '+%Y-%m-%d') \
 
 <b>User </b>: $GITLAB_USER_EMAIL \
